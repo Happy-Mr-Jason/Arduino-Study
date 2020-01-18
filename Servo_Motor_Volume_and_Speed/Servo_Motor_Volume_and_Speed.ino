4d22d60726fbe59523676585ec6bbb3ef79e71c1
Happy-Mr-Jason <happy.seongjin.project@gmail.com>
2020-01-19 03:57:16 +0900

    renew

--- Servo_Motor_Volume_and_Speed/Servo_Motor_Volume_and_Speed.ino
+++ Servo_Motor_Volume_and_Speed/Servo_Motor_Volume_and_Speed.ino
@@ -3,30 +3,39 @@ Servo myservo;
 #define VOL A0
 #define S_VOL A1
 
-int pos = 0 ; //angle
+int pos = 0;   //angle
 int speed = 0; //speed
-void setup() {
+void setup()
+{
   myservo.attach(6);
   Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
   pos = map(analogRead(VOL), 0, 1024, 0, 180);
   speed = map(analogRead(S_VOL), 0, 1024, 60, 0);
   Serial.println(String("Servo Position : ") + myservo.read() + String(" Target Position : ") + pos + String(" Speed : ") + speed + String(" msec/angle"));
-  if (myservo.read() < pos) {
+  if (myservo.read() < pos)
+  {
     Serial.println(String("Turnning Positive..."));
-    for (int i = myservo.read(); i <= pos ; i++) {
+    for (int i = myservo.read(); i <= pos; i++)
+    {
       myservo.write(i);
       delay(speed);
     }
-  } else if (myservo.read() > pos) {
+  }
+  else if (myservo.read() > pos)
+  {
     Serial.println(String("Turn Negativ..."));
-    for (int i = myservo.read(); i >= pos ; i--) {
+    for (int i = myservo.read(); i >= pos; i--)
+    {
       myservo.write(i);
       delay(speed);
     }
-  } else {
+  }
+  else
+  {
     Serial.println(String("Hold..."));
   }
 }
