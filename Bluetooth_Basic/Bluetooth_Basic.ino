af840a136fed3491fd13d3013b2a32527a587eb9
Happy-Mr-Jason <happy.seongjin.project@gmail.com>
2020-01-22 23:37:36 +0900

    Match to HC-06

--- Bluetooth_Basic/Bluetooth_Basic.ino
+++ Bluetooth_Basic/Bluetooth_Basic.ino
@@ -7,19 +7,19 @@ void setup()
     Serial.begin(9600);
     Serial.println("Arduino is ready");
     Serial.println("Remember to select Both NL & CR in the serial monitor");
-    // HC-05 default serial speed for AT mode is 38400
+    // HC-06 default serial speed for AT mode is 9600
     BTserial.begin(9600);
 }
 
 void loop()
 {
-    // Keep reading from HC-05 and send to Arduino Serial Monitor
+    // Keep reading from HC-06 and send to Arduino Serial Monitor
     if (BTserial.available())
     {
         c = BTserial.read();
         Serial.write(c);
     }
-    // Keep reading from Arduino Serial Monitor and send to HC-05
+    // Keep reading from Arduino Serial Monitor and send to HC-06
     if (Serial.available())
     {
         c = Serial.read();
