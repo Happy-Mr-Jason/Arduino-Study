85ce157a5f3cf8e798b14f6b71df62d0f77d35e9
Happy-Mr-Jason <happy.seongjin.project@gmail.com>
2020-02-10 23:49:48 +0900

    WiFi Test

--- WiFi_ESP8266_Basic/WiFi_ESP8266_Basic.ino
+++ WiFi_ESP8266_Basic/WiFi_ESP8266_Basic.ino
@@ -1,21 +1,19 @@
-#include <WiFi.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial WiFiSerial(3,2);
+SoftwareSerial WiFiSerial(2,3);
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   WiFiSerial.begin(115200);
 }
 
-void loop()
-{
-    if(Serial.available()){
-        WiFiSerial.write(Serial.read());
-    }
+void loop() {
+  if(Serial.available()){
+    WiFiSerial.write(Serial.read());
+  }
 
-    if(WiFiSerial.available()){
-        Serial.write(WiFiSerial.read());
-    }
+  if(WiFiSerial.available()){
+    Serial.write(WiFiSerial.read());
+  }
+
 }
