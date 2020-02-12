23bd9f2df4ca60b135348d4f11946f62813953fc
Happy-Mr-Jason <happy.seongjin.project@gmail.com>
2020-02-13 01:35:38 +0900

    20200213 Upgrade

--- 4wd_Car/4wd_Car.ino
+++ 4wd_Car/4wd_Car.ino
@@ -1,118 +1,231 @@
 #include <SoftwareSerial.h>
+#include <Servo.h>
 
-#define U1_TRIG 2
-#define U1_ECHO 3
-#define U2_TRIG 4
-#define U2_ECHO 5
-#define U3_TRIG 6
-#define U3_ECHO 7
-#define IN1 8
-#define IN2 9
-#define IN3 12
-#define IN4 13
-#define L_SPEED 10
-#define R_SPEED 11
+//Bluetooth Pins
+#define AD_TX_BT_RX 12
+#define AD_RX_BT_TX 13
+//Ultrasonic Sensor Pins
+#define UF_ECHO 2
+#define UF_TRIG 3
+#define UR_ECHO 4
+#define UR_TRIG 5
+#define UL_ECHO 6
+#define UL_TRIG 7
+//DC Motor controller(L298N) Pins
+#define M_IN1 11
+#define M_IN2 10
+#define M_IN3 9
+#define M_IN4 8
+#define L_SPEED A2
+#define R_SPEED A3
+//Servo Motor Pins
+#define S_BASE A0
+#define S_TILT A1
+
+//Bluetooth Communication
+SoftwareSerial BTSerial(AD_RX_BT_TX, AD_TX_BT_RX);
+//ServoMotors
+Servo mServo_B, mServo_T;
+//Base Rotation Limits
+int mServo_B_limit_p = 110;
+int mServo_B_limit_n = 70;
+//Tilter Tilting Limits
+int mServo_T_limit_p = 110;
+int mServo_T_limit_n = 70;
+//Operation Mode (0 : Manual, 1 : Auto)
+int operation_Mode = 0;
+//Distans variables (Unit : cm)
+int distance_F, distance_R, distance_L;
 
 void setup()
 {
     Serial.begin(9600);
-    pinMode(U1_TRIG, OUTPUT);
-    pinMode(U1_ECHO, INPUT);
-    pinMode(U2_TRIG, OUTPUT);
-    pinMode(U2_ECHO, INPUT);
-    pinMode(U3_TRIG, OUTPUT);
-    pinMode(U3_ECHO, INPUT);
-    pinMode(IN1, OUTPUT);
-    pinMode(IN2, OUTPUT);
-    pinMode(IN3, OUTPUT);
-    pinMode(IN4, OUTPUT);
+    BTSerial.begin(9600);
+    //Ultrasonic Sensors
+    pinMode(UF_TRIG, OUTPUT);
+    pinMode(UF_ECHO, INPUT);
+    pinMode(UR_TRIG, OUTPUT);
+    pinMode(UR_ECHO, INPUT);
+    pinMode(UL_TRIG, OUTPUT);
+    pinMode(UL_ECHO, INPUT);
+    //DC Motor Controller Pins
+    pinMode(M_IN1, OUTPUT);
+    pinMode(M_IN2, OUTPUT);
+    pinMode(M_IN3, OUTPUT);
+    pinMode(M_IN4, OUTPUT);
     pinMode(L_SPEED, OUTPUT);
     pinMode(R_SPEED, OUTPUT);
-
+    //Servo Motors
+    mServo_B.attach(S_BASE);
+    mServo_T.attach(S_TILT);
+    //Servo Motors Init Positioning (Center)
+    mServo_B.write(90);
+    mServo_T.write(90);
+    //Car Init Stop
     stopCar();
 }
 
 void loop()
 {
+    //Bluetooth Communication
+    bluetooth_Coms();
+    //Mesurement of Distance
+    distance_Front();
+    distance_Left();
+    distance_Right();
+
+    //Motor Controll
     analogWrite(L_SPEED, 255);
     analogWrite(R_SPEED, 255);
-    
-    // runCarforward();
-    // delay(2000);
+    // turnCarLeft();
     // stopCar();
-    
-    turnCarLeft();
-    delay(10000);
-    stopCar();
+    // turnCarRight();
+    // stopCar();
 
-    turnCarRight();
-    delay(10000);
-    stopCar();
-           
+    monitor_Serial();
 }
 
-void stopCar(){
+void bluetooth_Coms(){
+    //TODO///////////////////////////////////
+}
+
+void monitor_Serial()
+{
+    //Operation Mode
+    monitor_Seperator_Tag("Operation Mode");
+    Serial.println(operation_Mode == 1? String("Automatic mode") : String("Manual Mode"));
+    //Distance Monitoring
+    monitor_Seperator_Tag("Distance");
+    Serial.print(String("Front : ") + distance_F + String("cm"));
+    Serial.print(String(" / Left : ") + distance_L + String("cm"));
+    Serial.println(String(" / Right : ") + distance_R + String("cm"));
+    //Servo Motor Monitoring
+    monitor_Seperator_Tag("Servo Motors");
+    Serial.print(String("Base-Angle : ") + mServo_B.read() + char(' '));
+    Serial.println(String("Tilt-Angle : ") + mServo_T.read());
+}
+
+void monitor_Seperator_Tag(String monitor_name)
+{
+    Serial.print(char('[') + monitor_name + char(']'));
+    for (int i = 0; i < 50 - monitor_name.length(); i++)
+    {
+        if (i == 49 - monitor_name.length())
+        {
+            Serial.println(String("="));
+        }
+        else
+        {
+            Serial.print(String("="));
+        }
+    }
+}
+
+void distance_Front()
+{
+    digitalWrite(UF_TRIG, LOW);
+    delayMicroseconds(2);
+    digitalWrite(UF_TRIG, HIGH);
+    delayMicroseconds(2);
+    digitalWrite(UF_TRIG, LOW);
+    distance_F = (int)(pulseIn(UF_ECHO, HIGH) / 58.2);
+}
+
+void distance_Right()
+{
+    digitalWrite(UR_TRIG, LOW);
+    delayMicroseconds(2);
+    digitalWrite(UR_TRIG, HIGH);
+    delayMicroseconds(2);
+    digitalWrite(UR_TRIG, LOW);
+    distance_R = (int)(pulseIn(UR_ECHO, HIGH) / 58.2);
+}
+
+void distance_Left()
+{
+    digitalWrite(UL_TRIG, LOW);
+    delayMicroseconds(2);
+    digitalWrite(UL_TRIG, HIGH);
+    delayMicroseconds(2);
+    digitalWrite(UL_TRIG, LOW);
+    distance_L = (int)(pulseIn(UL_ECHO, HIGH) / 58.2);
+}
+
+void stopCar()
+{
     stopLeft();
     stopRight();
 }
 
-void runCarforward(){
+void runCarforward()
+{
     runRightForward();
     runLeftForward();
 }
 
-void runCarBackward(){
+void runCarBackward()
+{
     runRightBackward();
     runLeftBackward();
 }
 
-void turnCarLeft(){
+void turnCarLeft()
+{
     runRightForward();
 }
 
-void turnCarRight(){
+void turnCarRight()
+{
     runLeftForward();
 }
 
-void returnCarLeft(){
+void returnCarLeft()
+{
     runRightBackward();
 }
 
-void returnCarRight(){
+void returnCarRight()
+{
     runLeftBackward();
 }
 
-void runLeftForward(){
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, LOW);
+void runLeftForward()
+{
+    digitalWrite(M_IN1, HIGH);
+    digitalWrite(M_IN2, LOW);
     Serial.println("Left Forward");
 }
 
-void runLeftBackward(){
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, HIGH);
+void runLeftBackward()
+{
+    digitalWrite(M_IN1, LOW);
+    digitalWrite(M_IN2, HIGH);
     Serial.println("Left Backward");
 }
 
-void stopLeft(){
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, LOW);
+void stopLeft()
+{
+    digitalWrite(M_IN1, LOW);
+    digitalWrite(M_IN2, LOW);
     Serial.println("Left Stop");
 }
 
-void  runRightForward(){
-    digitalWrite(IN3, HIGH);
-    digitalWrite(IN4, LOW);
+void runRightForward()
+{
+    digitalWrite(M_IN3, HIGH);
+    digitalWrite(M_IN4, LOW);
     Serial.println("Right Forward");
 }
 
-void  runRightBackward(){
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, HIGH);
+void runRightBackward()
+{
+    digitalWrite(M_IN3, LOW);
+    digitalWrite(M_IN4, HIGH);
     Serial.println("Right Backward");
 }
-void stopRight(){
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, LOW);
+void stopRight()
+{
+    digitalWrite(M_IN3, LOW);
+    digitalWrite(M_IN4, LOW);
     Serial.println("Right Stop");
 }
